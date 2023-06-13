a68a8540def37ca377883f6b9daec384642a36f8
Montso Moalosi <montsoernestmoalosi@gmail.com>
2023-06-13 19:34:04 +0200

    Add Generate Random Quotes Logic in Javascript

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const allQoutes = [{
+const allQuotes = [{
     qoute: "I missed more than 9000 shots in my career. I lost almost 300 games. 26 times I been trusted to take the game winning shot and missed. I failed over and over and over again in my life. And that is why I succeed.",
     person: "Micheal Jordan"
 },
@@ -82,3 +82,19 @@ const allQoutes = [{
     qoute: "Success is not about being the best. It's about being better than you were yesterday.",
     person: "Unknown"
 }];
+
+const quoteDisplay = document.querySelector('.qoute p');
+const quoteAuthor = document.querySelector('.qoute h1');
+const generateBtn =  document.querySelector('.button');
+
+generateQoute();
+
+function generateQoute(){
+    const random = Math.floor(Math.random() * allQuotes.length);
+    console.log(random);
+    quoteDisplay.textContent = `"${allQuotes[random].qoute}"`;
+    quoteAuthor.textContent =  `- ${allQuotes[random].person}`;
+}
+
+generateBtn.addEventListener('click',generateQoute);
+
