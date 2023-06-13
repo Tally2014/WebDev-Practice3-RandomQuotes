97947350bc068359579be2fb3603ee76861bcf42
Montso Moalosi <montsoernestmoalosi@gmail.com>
2023-06-13 19:11:10 +0200

    Add qoutes object in JavaScript

---
+++ script.js
@@ -0,0 +1,84 @@
+const allQoutes = [{
+    qoute: "I missed more than 9000 shots in my career. I lost almost 300 games. 26 times I been trusted to take the game winning shot and missed. I failed over and over and over again in my life. And that is why I succeed.",
+    person: "Micheal Jordan"
+},
+{
+    qoute: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    person: "Winston Churchill"
+},
+{
+    qoute: "Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.",
+    person: "Albert Schweitzer"
+},
+{
+    qoute: "Success is not in what you have, but who you are.",
+    person: "Bo Bennett"
+},
+{
+    qoute: "Success usually comes to those who are too busy to be looking for it.",
+    person: "Henry David"
+},
+{
+    qoute: "The only place where success comes before work is in the dictionary.",
+    person: "Vidal Sassoon"
+},
+{
+    qoute: "Success is not the absence of failure; it's the persistence through failure.",
+    person: "Aisha Tyler"
+},
+{
+    qoute: "Success is walking from failure to failure with no loss of enthusiasm.",
+    person: "Winston Churchill"
+},
+{
+    qoute: "The secret of success is to know something nobody else knows.",
+    person: "Aristotle Onassis"
+},
+{
+    qoute: "Success is not just about making money. It's about making a difference.",
+    person: "Montso Moalosi"
+},
+{
+    qoute: "Success is not the key to success. Persistence and determination alone are omnipotent.",
+    person: "Calvin Coolidge"
+},
+{
+    qoute: "Success is not measured by what you accomplish, but by the opposition you have encountered and the courage with which you have maintained the struggle against overwhelming odds.",
+    person: "Orison Swett Marden"
+},
+{
+    qoute: "Success is not the result of spontaneous combustion; you must set yourself on fire.",
+    person: "Reggie Leach"
+},
+{
+    qoute: "Success is not the absence of problems; it's the ability to deal with them.",
+    person: "Steve Maraboli"
+},
+{
+    qoute: "Success is not just about reaching your goals; it's about enjoying the journey.",
+    person: "Zig Ziglar"
+},
+{
+    qoute: "Success is not about how many times you fall down but how many times you get back up.",
+    person: "Tally"
+},
+{
+    qoute: "The successful warrior is the average man, with laser-like focus.",
+    person: "Bruce Lee"
+},
+{
+    qoute: "Success is not the destination, it's the journey.",
+    person: "Zig Ziglar"
+},
+{
+    qoute: "The only limit to our realization of tomorrow will be our doubts of today.",
+    person: "Franklin D. Roosevelt"
+},
+{
+    qoute: "The difference between a successful person and others is not a lack of strength, not a lack of knowledge, but rather a lack of will.",
+    person: " Vince Lombardi"
+},
+{
+    qoute: "Success is not about being the best. It's about being better than you were yesterday.",
+    person: "Unknown"
+}];
